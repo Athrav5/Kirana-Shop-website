c8a213fb0c88a3d32361feb4f3aaf549991521c7
athravpatil <142832191+Athrav5@users.noreply.github.com>
2026-02-18 23:02:15 +0530

    Add files via upload

---
+++ kirana shop/first.js
@@ -0,0 +1,1 @@
+console.log("athrav patil")
---
+++ kirana shop/test.java
@@ -0,0 +1,7 @@
+class test {
+    public static void main(string arr[])
+    {
+        system.out.println("hello world");
+        system.out.print("welcome");
+    }
+}
